bb54ee2102d032f8b80e1cb42d1153c7f355704a
Your Name <aymanboukharraz@icloud.com>
2022-11-09 21:35:28 -0500

    flask app #4 - updated queries to return one

--- flask_app/flask04.py
+++ flask_app/flask04.py
@@ -6,6 +6,8 @@ from flask import request
 from flask import redirect, url_for
 from models import Note as Note
 from models import User as User
+from database import db
+
 
 
 
@@ -23,14 +25,14 @@ with app.app_context():
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
     my_notes = db.session.query(Note).all()
     return render_template('notes.html', notes=my_notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
-    my_note = db.session.query(Note).filter_by(id=note_id)
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
+    my_note = db.session.query(Note).filter_by(id=note_id).one()
     return render_template('note.html', note=my_note,user = a_user)
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -38,7 +40,7 @@ def get_note(note_id):
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+    a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
     return render_template('index.html', user = a_user)
 
 
@@ -62,7 +64,7 @@ def new_note():
        
         return redirect(url_for('get_notes'))
     else:
-        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu')
+        a_user = db.session.query(User).filter_by(email='mogli@uncc.edu').one()
         return render_template('new.html', user = a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
